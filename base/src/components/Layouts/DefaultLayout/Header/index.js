c995e16ff00dd9b838a3f660248391b2fcddd499
lmha@cmc.com.vn <lmha@cmc.com.vn>
2022-06-08 15:59:44 +0700

    header, Search bar

--- base/src/components/Layouts/DefaultLayout/Header/index.js
+++ base/src/components/Layouts/DefaultLayout/Header/index.js
@@ -1,7 +1,46 @@
+import React, { useState } from 'react';
 import Style from './Header.module.scss';
+import className from 'classnames/bind';
+import image from '~/assets/images';
+import Tippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner, faCircleXmark, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
+const cx = className.bind(Style);
 function Header() {
-    return <div>Header</div>;
+    const [searchResult, setSearchResult] = useState([]);
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={image.logo} />
+                </div>
+                <Tippy
+                    interactive={true}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>Keysadaaaaaaaaaa</PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search-container')}>
+                        <input placeholder="seach title" spellCheck={false} />
+                        <button className={cx('clear-btn')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={cx('actions')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
